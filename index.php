4f20a2de81e7e5742b70277dd6a8f031cdaba174
iskerrett <iskerrett>
2010-03-15 18:23:39 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 	
 	# Define these here, or in _projectCommon.php for site-wide values
 	$pageKeywords	= "eclipse, project";
-	$pageAuthor		= "Your name";
-	$pageTitle 		= "My project home";
+	$pageAuthor		= "Ian Skerrett";
+	$pageTitle 		= "Marketplace Client";
 	
 	
 	// 	# Paste your HTML content between the EOHTML markers!
