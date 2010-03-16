b01ddeab2d42d90694061f0ec9603e0f4f8c3f83
spingel <spingel>
2010-03-16 17:01:41 +0000

    update page

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,20 +1,8 @@
 <?php
 
-/*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    
- *******************************************************************************/
-
 	# Set the theme for your project's web pages.
 	# See http://eclipse.org/phoenix/
 	$theme = "Nova";
-	
 
 	# Define your project-wide Navigation here
 	# This appears on the left of the page if you define a left nav
@@ -22,29 +10,21 @@
 	# these are optional
 	
 	# If you want to override the eclipse.org navigation, uncomment below.
-	# $Nav->setLinkList(array());
-	
-	# Break the navigation into sections
-	$Nav->addNavSeparator("Marketplace Client ", 	"/mpc");
-//	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
-//	$Nav->addCustomNav("Documentation", "/project/documentation.php", "_blank", 3);
-//	$Nav->addCustomNav("Support", "/project/support.php", "_blank", 3);
-//	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
+	$Nav->setLinkList(array());
+	$Nav->addNavSeparator("Marketplace Client ", "/mpc", "_self", 3);
+	$Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=technology.packaging.mpc", "_self", 3);
+	$Nav->addCustomNav("Getting Involved", "http://wiki.eclipse.org/EPP/MPC/Contributor_Guide", "_self", 3);
 
 	# Define keywords, author and title here, or in each PHP page specifically
 	$pageKeywords	= "marketplace, client, p2, installer";
-	$pageAuthor		= "Your name";
-	# $pageTitle 		= "Xtext";
-
+	$pageAuthor		= "Ian Skerrett";
+	$pageTitle 		= "Marketplace Client";
 
 	# top navigation bar
 	# To override and replace the navigation with your own, uncomment the line below.
-	# $Menu->setMenuItemList(array());
-	# $Menu->addMenuItem("Home", "/project", "_self");
-	# $Menu->addMenuItem("Download", "/project/download.php", "_self");
-	# $Menu->addMenuItem("Documentation", "/project/documentation.php", "_self");
-	# $Menu->addMenuItem("Support", "/project/support.php", "_self");
-	# $Menu->addMenuItem("Developers", "/project/developers", "_self");
+	$Menu->setMenuItemList(array());
+	$Menu->addMenuItem("Home", "/mpc", "_self");
+	$Nav->addCustomNav("Getting Involved", "http://wiki.eclipse.org/EPP/MPC/Contributor_Guide", "_self", 3);
 	
 	# To define additional CSS or other pre-body headers
 	# $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
@@ -54,4 +34,5 @@
 	
 	# If you have Google Analytics code, use it here
 	# $App->SetGoogleAnalyticsTrackingCode("YOUR_CODE");
+	
 ?>
